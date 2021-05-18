47195296e5496c68345b7b70f0ffc8680820965e
nikolina1901 <72207121+nikolina1901@users.noreply.github.com>
2021-05-18 10:29:50 +0200

    fixed countrie list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [countryDetails, setCountryDetails] = useState("Afghanistan");
   useEffect(() => {
     fetchedCountries();
   });
+
   const fetchedCountries = () => {
     axios
       .get("https://restcountries.eu/rest/v2/all")
-      .then((response) => console.log(response))
+      .then((response) => setCountries(response.data))
       .catch((error) => console.log(error));
   };
-
-  return <div className="App"></div>;
+  const handleOnChange = (event) => {
+    setCountryDetails(event.target.value);
+  };
+  return (
+    <div className="App">
+      <select onChange={handleOnChange}>
+        {countries.map((item) => (
+          <option key={item.name} value={item.name}>
+            {item.name}
+          </option>
+        ))}
+      </select>
+      {/* display country info */}
+      {countries
+        .filter((country) => country.name === countryDetails)
+        .map((item) => (
+          <div>
+            <h1>{item.name}</h1>
+            <h2>{item.capital}</h2>
+            <p>{item.population}</p>
+            <img src={item.flag} alt="flag" width="100" />
+            {item.languages.map((language) => (
+              <div key={language.name}>
+                <p>{language.name}</p>
+                <p>{language.nativeName}</p>
+              </div>
+            ))}
+            {item.currencies.map((curency) => (
+              <div key={curency.name}>
+                <p>{curency.name}</p>
+                <p>{curency.code}</p>
+                <p>{curency.symbol}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default App;
